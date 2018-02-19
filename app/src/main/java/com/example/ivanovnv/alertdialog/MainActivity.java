8ddc30cef3b3eb5abc49033a690b4528e277a35b
alien1900 <alien1900@bk.ru>
2018-02-19 11:37:11 +0700

    finale

--- app/src/main/java/com/example/ivanovnv/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/ivanovnv/alertdialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ivanovnv.alertdialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo();
+            }
+        });
 
     }
 
     private void showInfo () {
-        
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Info")
+                .setMessage("London is the capital of Great Britain")
+                .setPositiveButton("true", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "You absolutely right", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("false", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Wrong",Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+
     }
 }
